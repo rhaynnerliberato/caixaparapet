cba1577deb6f08eb4e28e49744521718c4207ca3
Rhaynner Liberato <rhaynnerliberato@MacBook-Pro-de-Rhaynner.local>
2022-05-21 12:18:22 -0300

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CaixaParaPetComponent } from 'src/app/caixa-para-pet/caixa-para-pet.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {
+    path: '',
+    component: CaixaParaPetComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,64 @@
-import { NgModule } from '@angular/core';
+import { DEFAULT_CURRENCY_CODE, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatStepperModule } from '@angular/material/stepper';
+import { MatButtonModule } from '@angular/material/button';
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatNativeDateModule, MAT_DATE_LOCALE } from '@angular/material/core';
+import {MatCardModule} from '@angular/material/card';
+import { IniciarOrcamentoComponent } from 'src/app/caixa-para-pet/iniciar-orcamento/iniciar-orcamento.component';
+import { TipoCaixaComponent } from 'src/app/caixa-para-pet/tipo-caixa/tipo-caixa.component';
+import { AlturaDataComponent } from 'src/app/caixa-para-pet/altura-data/altura-data.component';
+import { FinalizarPedidoComponent } from 'src/app/caixa-para-pet/finalizar-pedido/finalizar-pedido.component';
+import { CaixaParaPetComponent } from 'src/app/caixa-para-pet/caixa-para-pet.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CaixaParaPetComponent,
+    HeaderComponent,
+    FooterComponent,
+    IniciarOrcamentoComponent,
+    TipoCaixaComponent,
+    AlturaDataComponent,
+    FinalizarPedidoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatBadgeModule,
+    MatIconModule,
+    MatButtonModule,
+    MatMenuModule,
+    MatProgressBarModule,
+    MatExpansionModule,
+    MatStepperModule,
+    ReactiveFormsModule,
+    FormsModule,
+    MatDatepickerModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatNativeDateModule,
+    MatCardModule,
+
   ],
-  providers: [],
+  providers: [
+    {provide: MAT_DATE_LOCALE, useValue: 'pt-BR'},
+    { provide: DEFAULT_CURRENCY_CODE, useValue: 'BRL' },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
