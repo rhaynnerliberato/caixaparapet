84857cebd5f325eb4e96c5bbede0eb7c9ff46d90
Rhaynner Liberato <rhaynnerliberato@MacBook-Pro-de-Rhaynner.local>
2022-05-29 22:47:56 -0300

    testantiva de server

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ const PORT = process.env.PORT || 8080;
 
 app.use(express.static(__dirname + '/dist/singlepage'));
 
-app.get('/*', (req, res) => (
-  res.sendFile(__dirname + '/dist/singlepage/index.html')
+app.get('/', (req, res) => (
+  res.send(__dirname + '/dist/singlepage/index.html')
 ));
 
 app.listen(PORT, ()=> {
